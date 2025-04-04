2e4e7f207a9125f262932995dbed12705c30bcad
barbpic <cementbitengo19@gmail.com>
2025-04-04 15:40:42 +0300

    wk2 assignment

---
+++ python/ASSIGNMENTS/my_list.py
@@ -0,0 +1,18 @@
+
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+index_20 = my_list.index(20)
+print("Index of 20:", index_20)
